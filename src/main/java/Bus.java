449bc6b433e6ef580fad4269e0fb0fadee7cc709
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-21 14:57:06 +0100

    Add test for busStartEmpty

---
+++ src/main/java/Bus.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Bus {
+    //Destination, Capacity and an initially empty ArrayList passengers of type Person
+    private String destination;
+    private int capacity;
+    private ArrayList<Person> passengers;
+
+    public int personCount() {
+        return 0;
+    }
+}
---
+++ src/test/java/BusTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusTest {
+
+private Bus bus;
+
+@Before
+public void before() {
+  bus = new Bus();
+}
+
+@Test
+public void busStartsEmpty() {
+  assertEquals(0, bus.personCount());
+}
+
+}
