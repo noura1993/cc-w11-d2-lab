d4883b342eee3f27e6e7ae570a9e5f34573bef47
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-21 15:21:44 +0100

    Add test for addPerson

---
+++ src/main/java/BusStop.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class BusStop {
+    private ArrayList<Person> queue;
+    private String name;
+
+    public BusStop(String name) {
+        this.name = name;
+        this.queue = new ArrayList<Person>();
+    }
+
+    public int personCount() {
+        return this.queue.size();
+    }
+
+    public void addPerson(Person person1) {
+        this.queue.add(person1);
+    }
+}
---
+++ src/test/java/BusStopTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusStopTest {
+
+    private BusStop stop;
+    private Bus bus;
+    private Person person1, person2, person3;
+
+    @Before
+    public void before() {
+        stop = new BusStop("codeclan stop");
+        bus = new Bus("codeclan", 2);
+        person1 = new Person();
+        person2 = new Person();
+        person3 = new Person();
+        stop.addPerson(person1);
+        stop.addPerson(person2);
+        stop.addPerson(person3);
+    }
+
+    @Test
+    public void addPerson() {
+        stop.addPerson(person1);
+        assertEquals(4, stop.personCount());
+    }
+}
